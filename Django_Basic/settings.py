ba99fd1c16000d77a9e0237af0079f67c4268790
“Hedward <trab.hedward@gmail.com>
2022-05-26 21:28:04 -0300

    Second commit, nothing special, just configuring the settings.py and started to see views

--- Django_Basic/settings.py
+++ Django_Basic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$khn%m9%y9a+*o#9fxfr+d*8sh$tqqz*^vu@myf9-^mo#h^%3=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
