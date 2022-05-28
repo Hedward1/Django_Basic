72278fd25082b1e2371e9507cc496ba2ac6f6819
“Hedward <trab.hedward@gmail.com>
2022-05-28 15:44:38 -0300

    Files admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from core.models import Product, Client
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'email')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Client, ClientAdmin)
+
+
--- core/models.py
+++ core/models.py
@@ -6,10 +6,18 @@ class Product(models.Model):
     price = models.DecimalField('Price', decimal_places=2, max_digits=8)
     quantity = models.IntegerField('Inventory quantity')
 
+    def __str__(self):
+        return self.name
+
 
 class Client(models.Model):
     name = models.CharField('Name', max_length=100)
     last_name = models.CharField('last_name', max_length=100)
     email = models.CharField('email', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 
 
