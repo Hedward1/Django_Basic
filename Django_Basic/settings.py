e7ca2e74f7232bd577771c50d92f8d2a3eb36d7e
“Hedward <trab.hedward@gmail.com>
2022-05-28 11:33:50 -0300

    Templates in Django

--- Django_Basic/settings.py
+++ Django_Basic/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
 
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'course': 'Dev with Django framework',
+        'other': 'Django is Great !'
+    }
+    return render(request, 'index.html', context)
 
 
 def concact(request):
