6e583bc4d8c83959b698e9f4f95cc3e2f1f3c051
Hedward <trab.hedward@gmail.com>
2022-06-01 17:35:09 -0300

    python querys

--- core/views.py
+++ core/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 
 
 def index(request):
-    print(dir(request))
-    print(f'method: {request.method}')
-    print(f'method: {request.headers}')
+    print('------------------------------------------------------------------------------------------------------')
+    print(f'{dir(request)}')
+    print(f'Method: {request.method}')
+    #(f"headers: {request.headers}")
+    print('------------------------------------------------------------------------------------------------------')
+    print(f"Dir Requests: {dir(request.user)}")
+    print(f"user tests: {request.user.user_permissions}")
+
+    if str(request.user) == 'AnonymousUser':
+        login = 'The user is anonymous'
+    else:
+        login = f'the user is: {request.user}'
 
     context = {
         'course': 'Dev with Django framework',
-        'other': 'Django is Great !'
+        'other': 'Django is Great !',
+        'login': login
     }
     return render(request, 'index.html', context)
 
