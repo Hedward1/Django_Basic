5b4cfeb52ba03c6ab46a33e791f046ff3e306b69
Hedward <trab.hedward@gmail.com>
2022-06-01 09:47:06 -0300

    Learning about requests

--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request))
+    print(f'method: {request.method}')
+    print(f'method: {request.headers}')
+
     context = {
         'course': 'Dev with Django framework',
         'other': 'Django is Great !'
